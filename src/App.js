1732dd0073e129ba0317ff6cbe66b01b3dad964c
Shubham Shandilya <shubhamshandilya05@gmail.com>
2024-07-16 09:34:29 +0530

    Commiting files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import DSAList from "./components/DSAList.js"; 
+<script src="https://cdn.tailwindcss.com"></script>
+export default function App() { 
+    return ( 
+        <> 
+            <div className="flex flex-col justify-center items-center mt-4"> 
+                <h1 className="text-blue-800 font-bold text-7xl"> 
+                    Pro-Track-Ter 
+                </h1> 
+                <h3 className="bg-clip-text text-transparent bg-blue-500 font-bold text-3xl mb-4"> 
+                    DSA Tracker 
+                </h3> 
+                <DSAList /> 
+            </div> 
+        </> 
+    ); 
 }
-
-export default App;
